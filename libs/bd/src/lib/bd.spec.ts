cb387273fc540484e5d4ecdb2e5528197e80248e
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2022-07-24 12:45:55 -0300

    Generate BD lib

---
+++ libs/bd/src/lib/bd.spec.ts
@@ -0,0 +1,7 @@
+import { bd } from "./bd";
+
+describe("bd", () => {
+  it("should work", () => {
+    expect(bd()).toEqual("bd");
+  });
+});
---
+++ libs/bd/src/lib/bd.ts
@@ -0,0 +1,3 @@
+export function bd(): string {
+  return "bd";
+}
