55f20198770f609a6d69d9647007f87c5d55100d
Kevin Souto <soutokevin@gmail.com>
2022-09-05 08:54:23 -0300

    Corriido erro de typescript

--- apps/back/src/repositories/s3.ts
+++ apps/back/src/repositories/s3.ts
@@ -1,13 +1,14 @@
+import { Readable } from "node:stream"
+import { Response } from "express"
 import {
   S3Client,
   PutObjectCommand,
   GetObjectCommand,
 } from "@aws-sdk/client-s3"
-import { Response } from "express"
 
 const key_id = process.env.S3_ACCESS_KEY_ID
 const secret = process.env.S3_ACCESS_SECRET
-const bucket = process.env.S3_BUCKET
+const Bucket = process.env.S3_BUCKET
 const region = process.env.S3_REGION
 
 const s3 = new S3Client({
@@ -15,22 +16,14 @@ const s3 = new S3Client({
   credentials: { accessKeyId: key_id, secretAccessKey: secret },
 })
 
-export async function store(name: string, body: string | Uint8Array | Buffer) {
-  const command = new PutObjectCommand({
-    Bucket: bucket,
-    Key: name,
-    Body: body,
-  })
-
+export async function store(name: string, Body: string | Uint8Array | Buffer) {
+  const command = new PutObjectCommand({ Bucket, Body, Key: name })
   await s3.send(command)
 }
 
-export async function retrieve(name: string, res: Response) {
-  const command = new GetObjectCommand({
-    Bucket: bucket,
-    Key: name,
-  })
-
+export async function retrieve(res: Response, name: string) {
+  const command = new GetObjectCommand({ Bucket, Key: name })
   const response = await s3.send(command)
-  response.Body.pipe(res)
+  const body = response.Body as Readable
+  body.pipe(res)
 }
