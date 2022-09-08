ed60f0e38e66dcea2f7150360c267091180ed7d9
JomaSnow <jm.duda@uol.com.br>
2022-09-08 12:13:31 -0300

    Mocks some data and starts to build calculation function

---
+++ apps/front/components/CalculatorList.tsx
@@ -0,0 +1,110 @@
+import React from "react"
+import styled from "styled-components"
+import ListItem from "./ListItems"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPlus } from "@fortawesome/free-solid-svg-icons"
+
+// =================================== Begin Styles =====================================================
+
+const Body = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 330px;
+  border-radius: 30px;
+  background-color: #ffffff;
+`
+
+const ListHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 90%;
+  flex: 1;
+  border-bottom: 1px solid lightgray;
+  padding: 0 1rem;
+  margin-bottom: 15px;
+
+  svg {
+    cursor: pointer;
+  }
+`
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-self: flex-end;
+`
+
+const Title = styled.h2`
+  font-size: 1.5rem;
+  font-weight: 500;
+`
+
+const Label = styled.span`
+  font-size: 0.6rem;
+`
+
+const ListScroll = styled.div`
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+  width: 90%;
+  flex: 4;
+`
+// =================================== End of Styles =====================================================
+
+const CalculatorList = ({ consumoList = [] }) => {
+  return (
+    <>
+      <Body>
+        <ListHeader>
+          <HeaderWrapper>
+            <Title>CONSUMOS INFORMADOS</Title>
+            <Label>Total: {consumoList.length}</Label>
+          </HeaderWrapper>
+          <FontAwesomeIcon icon={faPlus} color="#0081CF" size="2x" />
+        </ListHeader>
+        <ListScroll>
+          {consumoList.length <= 0 && <span>Adicione um consumo</span>}
+          {consumoList.map((c) => {
+            let type = 0
+            try {
+              if (c.timesPressed > 0) {
+                type = 1
+              }
+            } catch (error) {
+              type = 0
+            }
+            if (type === 0) {
+              return (
+                <ListItem
+                  type={0}
+                  key={c.id}
+                  modelName={c.name}
+                  minutesPressed={c.minutesPressed}
+                />
+              )
+            }
+            if (type === 1) {
+              return (
+                <ListItem
+                  type={1}
+                  key={c.id}
+                  modelName={c.name}
+                  descargaType={c.type[0]}
+                  timesPressed={c.timesPressed}
+                />
+              )
+            }
+          })}
+        </ListScroll>
+      </Body>
+    </>
+  )
+}
+
+export default CalculatorList
---
+++ apps/front/components/ListItems.tsx
@@ -0,0 +1,97 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faX } from "@fortawesome/free-solid-svg-icons"
+import styled from "styled-components"
+import DefaultWaterImage from "../assets/waterDefaultImage.jpg"
+import Image from "next/image"
+
+const Body = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 10px 20px;
+  border: 3px solid #336666;
+  border-radius: 30px;
+  background-color: #33666633;
+  width: 100%;
+  height: 110px;
+  margin: 10px 0;
+
+  div {
+    align-items: flex-start;
+    margin: 0 5px;
+    display: flex;
+    flex-direction: column;
+  }
+`
+const StyledImg = styled(Image)`
+  width: 100%;
+  height: 100%;
+  border: 1px solid #336666;
+  border-radius: 100%;
+`
+const Label = styled.span`
+  margin: 0;
+  font-size: 0.7rem;
+  font-weight: 200;
+  margin-bottom: -4px;
+`
+
+const Title = styled.span`
+  margin: 0;
+  font-size: 1.4rem;
+  font-weight: 500;
+`
+
+const Value = styled.span`
+  margin-bottom: 5px;
+  font-size: 0.9rem;
+  font-weight: 500;
+  :last-of-type {
+    margin: 0;
+  }
+`
+const ListItem = ({
+  type,
+  modelName = "Model XPTO nome grande",
+  descargaType = { seconds: 0, totalWaterCost: 0 },
+  timesPressed = 0,
+  waterPerMinute = 0,
+  minutesPressed = 0,
+}) => {
+  return (
+    <>
+      <Body>
+        <div style={{ flex: 1 }}>
+          <StyledImg src={DefaultWaterImage} alt="model image" />
+        </div>
+        <div style={{ flex: 2 }}>
+          <Label>{type === 0 ? "Chuveiro" : "Descarga"}</Label>
+          <Title>{modelName}</Title>
+        </div>
+        <div style={{ flex: 1, alignSelf: "flex-end", alignItems: "center" }}>
+          {type === 0 && (
+            <>
+              <Label>Ligado Por</Label>
+              <Value>{minutesPressed} min</Value>
+            </>
+          )}
+          {type === 1 && (
+            <>
+              <Label>Acionado</Label>
+              <Value>{timesPressed} vezes</Value>
+              <Label>Durante</Label>
+              <Value>{descargaType.seconds} segs</Value>
+            </>
+          )}
+        </div>
+        <FontAwesomeIcon
+          icon={faX}
+          color="red"
+          size="2xs"
+          style={{ cursor: "pointer" }}
+        />
+      </Body>
+    </>
+  )
+}
+
+export default ListItem
